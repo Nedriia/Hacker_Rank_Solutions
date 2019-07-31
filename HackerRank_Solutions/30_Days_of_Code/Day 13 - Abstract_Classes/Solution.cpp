0f8f6565324cf0261b0772673e00929666d124e7
Nedriia <arthur.favre@hotmail.com>
2019-07-31 14:48:04 +0200

    Update Solution.cpp

--- HackerRank_Solutions/30_Days_of_Code/Day 13 - Abstract_Classes/Solution.cpp
+++ HackerRank_Solutions/30_Days_of_Code/Day 13 - Abstract_Classes/Solution.cpp
@@ -52,3 +52,12 @@ private:
 // End class
 
 int main() {
+    string title,author;
+    int price;
+    getline(cin,title);
+    getline(cin,author);
+    cin>>price;
+    MyBook novel(title,author,price);
+    novel.display();
+    return 0;
+}
