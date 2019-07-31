28e82db9197bff4cc54ee8587422316e739b1bb7
Nedriia <arthur.favre@hotmail.com>
2019-07-31 14:50:28 +0200

    Update Solution.cpp

--- HackerRank_Solutions/30_Days_of_Code/Day 17 - More_Exceptions/Solution.cpp
+++ HackerRank_Solutions/30_Days_of_Code/Day 17 - More_Exceptions/Solution.cpp
@@ -17,3 +17,20 @@ struct Calculator
 
 
 int main()
+{
+    Calculator myCalculator=Calculator();
+    int T,n,p;
+    cin>>T;
+    while(T-->0){
+      if(scanf("%d %d",&n,&p)==2){
+         try{
+               int ans=myCalculator.power(n,p);
+               cout<<ans<<endl; 
+         }
+         catch(exception& e){
+             cout<<e.what()<<endl;
+         }
+      }
+    }
+    
+}
