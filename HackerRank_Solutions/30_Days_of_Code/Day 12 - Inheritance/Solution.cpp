9ffc1596d1d71cea5cf0bf785f4eda5c98067a21
Nedriia <arthur.favre@hotmail.com>
2019-07-31 14:47:03 +0200

    Update Solution.cpp

--- HackerRank_Solutions/30_Days_of_Code/Day 12 - Inheritance/Solution.cpp
+++ HackerRank_Solutions/30_Days_of_Code/Day 12 - Inheritance/Solution.cpp
@@ -70,4 +70,20 @@ public:
 	// Write your function here
 };
 
-int main() {
+nt main() {
+	string firstName;
+  	string lastName;
+	int id;
+  	int numScores;
+	cin >> firstName >> lastName >> id >> numScores;
+  	vector<int> scores;
+  	for(int i = 0; i < numScores; i++){
+	  	int tmpScore;
+	  	cin >> tmpScore;
+		scores.push_back(tmpScore);
+	}
+	Student* s = new Student(firstName, lastName, id, scores);
+	s->printPerson();
+	cout << "Grade: " << s->calculate() << "\n";
+	return 0;
+}
