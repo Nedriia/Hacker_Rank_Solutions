32433c99814051e5443fc621d8dfed022e853d60
Nedriia <arthur.favre@hotmail.com>
2019-07-31 14:48:52 +0200

    Update Solution.cpp

--- HackerRank_Solutions/30_Days_of_Code/Day 14 - Scope/Solution.cpp
+++ HackerRank_Solutions/30_Days_of_Code/Day 14 - Scope/Solution.cpp
@@ -31,3 +31,25 @@ public:
 	// Add your code here
 
 }; // End of Difference class
+
+int main() {
+    int N;
+    cin >> N;
+    
+    vector<int> a;
+    
+    for (int i = 0; i < N; i++) {
+        int e;
+        cin >> e;
+        
+        a.push_back(e);
+    }
+    
+    Difference d(a);
+    
+    d.computeDifference();
+    
+    cout << d.maximumDifference;
+    
+    return 0;
+}
